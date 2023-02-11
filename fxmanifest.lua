969192e90e87eb996ef47e8e8c984523bbc345a9
3wa <117110933+3waDev@users.noreply.github.com>
2023-02-11 22:14:38 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Ewaツ#5334'
+description 'tie a door at specific bank'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_script 'client.lua'
+
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,23 @@
+ESX = exports["es_extended"]:getSharedObject()
+
+ESX.RegisterServerCallback('esx_tiesdoor:ties', function(source, cb)
+
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    local TiesQuantity = xPlayer.getInventoryItem('ties').count
+
+    if TiesQuantity > 0 then
+        cb(true)
+        xPlayer.removeInventoryItem('ties', 1)
+    else
+        cb(false)
+        TriggerClientEvent('esx:showNotification', xPlayer.source, '~r~You dont have a plastic ties!')
+    end    
+end)
+
+-- For Sync the client --
+RegisterServerEvent("esx_tiesdoor:server:syncdoor")
+AddEventHandler("esx_tiesdoor:server:syncdoor", function(x,y,z,hash,yaw,type)
+    TriggerClientEvent("esx_tiesdoor:client:syncdoor", -1, x,y,z,hash,yaw,type)
+end)
+-------------------------
